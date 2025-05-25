a159768d1db26b913a86405a0d1e30f784fa90b7
MoonProger <danchenkonadya.dn8@gmail.com>
2025-05-25 10:03:27 +0300

    Add design file

---
+++ Dockerfile
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:1b0c0831df76931ad11a3aca5af6400f088b192332f21d1ceb023c90c4261da6
+size 258
---
+++ main.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:ecf43e032c84481d26c23af2318e864cafd4ed224e6ee8424128a2d7cf300b87
+size 1075
